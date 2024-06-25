2960acc46af6908eb41156418617e8b3bcaa7647
Vignesh Jayabalan <vignesh.jayabalan@hashicorp.com>
2024-06-25 14:54:14 +0530

    Add docs generation

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -5,15 +5,15 @@ terraform {
     }
   }
 }
+
 provider "tfm" {
   github_token = "token"
 }
 
-
 resource "tfm_directory_actions" "terraform-migration-spikes" {
   directory_path = "/Users/jitendra/hashi-code/terraform-migration-spikes"
-  org = "jitendra-org"
-  project = "migration"
-  workspace = "default"
+  org            = "jitendra-org"
+  project        = "migration"
+  workspace      = "default"
   git_commit_msg = "[SKIP CI] migrated sample-project's default directory"
 }
---
+++ examples/resources/tfmigrate_terraform_plan/resource.tf
@@ -0,0 +1,5 @@
+
+# Terraform Plan Resource Example
+resource "tfmigrate_terraform_plan" "plan_example" {
+  directory_path = "/path/to/terraform/configs"
+}
