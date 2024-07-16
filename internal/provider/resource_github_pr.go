0491d0522a17cc09edc17ef0d5e8d24afee93e57
Vignesh Jayabalan <vignesh.jayabalan@hashicorp.com>
2024-07-16 13:20:59 +0530

    Missed commit on PR URl

--- internal/provider/resource_github_pr.go
+++ internal/provider/resource_github_pr.go
@@ -30,6 +30,7 @@ type GithubPrModel struct {
 	SourceBranch   types.String `tfsdk:"source_branch"`
 	DestinBranch   types.String `tfsdk:"destin_branch"`
 	Summary        types.String `tfsdk:"summary"`
+	PrUrl          types.String `tfsdk:"pull_request_url"`
 }
 
 func (r *githubPr) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -41,24 +42,28 @@ func (r *githubPr) Schema(_ context.Context, _ resource.SchemaRequest, resp *res
 		MarkdownDescription: "Github Pr Resource: This resource is used to create PR on github.",
 		Attributes: map[string]schema.Attribute{
 			"repo_identifier": schema.StringAttribute{
-				MarkdownDescription: "The directory path where Git Commit needs to be executed.",
+				MarkdownDescription: "The identifier of the repository in the format `owner/repo`.",
 				Required:            true,
 			},
 			"pr_title": schema.StringAttribute{
-				MarkdownDescription: "The commit message that needs to be used for the commit.",
+				MarkdownDescription: "The PR title.",
 				Required:            true,
 			},
 			"pr_body": schema.StringAttribute{
-				MarkdownDescription: "The commit message that needs to be used for the commit.",
+				MarkdownDescription: "Content of the PR Body.",
 				Required:            true,
 			},
 			"source_branch": schema.StringAttribute{
-				MarkdownDescription: "The commit message that needs to be used for the commit.",
+				MarkdownDescription: "The feature branch from which the PR will be merged into",
 				Required:            true,
 			},
 			"destin_branch": schema.StringAttribute{
-				MarkdownDescription: "The commit message that needs to be used for the commit.",
+				MarkdownDescription: "The Base branch into which the PR will be merged into",
 				Required:            true,
+			},
+			"pull_request_url": schema.StringAttribute{
+				MarkdownDescription: "The URL of the Pull Request created.",
+				Computed:            true,
 			},
 			"summary": schema.StringAttribute{
 				MarkdownDescription: "Summary of the Git Commit Resource.",
@@ -86,6 +91,7 @@ func (r *githubPr) Create(ctx context.Context, req resource.CreateRequest, resp 
 	prUrl, err := gitops.CreatePullRequest(repoIdentifier, destinBranch, sourceBranch, prTitle, prBody, githubToken)
 
 	data.Summary = types.StringValue("Github PR Created: " + prUrl)
+	data.PrUrl = types.StringValue(prUrl)
 	if err != nil {
 		tflog.Error(ctx, "Error creating PR: "+err.Error())
 		resp.Diagnostics.AddError("Error creating PR: ", err.Error())
